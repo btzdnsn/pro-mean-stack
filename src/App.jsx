2cd49f40bb4c56aba5c9c0a95fba27525a982499
codemarker <942850532@qq.com>
2017-03-28 16:22:37 +0800

    communicateing form child to parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,227 @@
 const contentNode = document.getElementById('contents');
 
-class IssueList extends React.Component {
+
+// In-Memory Issue List
+const issues = [
+	{
+		id: 1, 
+		status: 'Open', 
+		owner: 'Ravan',
+		created: new Date('2016-08-15'), 
+		effort: 5,
+		completionDate: undefined,
+		title: 'Error in console when clicking Add'
+	},
+	{
+		id: 2, 
+		status: 'Assigned', 
+		owner: 'Eddie',
+		created: new Date('2016-08-16'), 
+		effort: 14,
+		completionDate: new Date('2016-08-30'),
+		title: 'Missing bottom border on panel'
+	}
+];
+
+
+
+
+
+
+
+// BorderWrap组件
+/*class BorderWrap extends React.Component {
+	render() {
+		const borderedStyle = {
+			border: '1px solid silver',
+			padding: 6
+		};
+
+		return (
+			<div style={borderedStyle}>
+				{this.props.children}
+			</div>
+		);
+	}
+}
+*/
+
+
+class IssueFilter extends React.Component {
 	render() {
 		return (
-			<div>This is a placeholder for the issue list.</div>
+			<div>This is a placeholder for the Issue Filter.</div>
+		);
+	}
+}
+
+
+
+class IssueRow extends React.Component {
+	// static get defaultProps() {
+	// 	return {
+	// 		issue_title: '-- no title --',
+	// 	}	
+	// }
+
+	// static get propTypes() {
+	// 	return {
+	// 		issue_id: React.PropTypes.number.isRequired,
+	// 		issue_title: React.PropTypes.string
+	// 	}
+	// }
+
+	render() {
+		const issue = this.props.issue;
+
+		return (
+			<tr>
+				<td>{issue.id}</td>
+				<td>{issue.status}</td>
+				<td>{issue.owner}</td>
+				<td>{issue.created.toDateString()}</td>
+				<td>{issue.effort}</td>
+				<td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+				<td>{issue.title}</td>
+			</tr>
+		);
+	}
+}
+
+
+class IssueTable extends React.Component {
+	render() {
+		const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+
+		return (
+			<table className="bordered-table">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Status</th>
+						<th>Owner</th>
+						<th>Created</th>
+						<th>Effort</th>
+						<th>Completion Date</th>
+						<th>Title</th>
+					</tr>
+				</thead>
+				<tbody>
+					{issueRows}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+
+
+
+
+// IssueAdd组件
+class IssueAdd extends React.Component {
+	constructor() {
+		super();
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(e) {
+		// Prevent Default Behaviour
+		e.preventDefault();
+		var form = document.forms.issueAdd;
+		this.props.createIssue({
+			owner: form.owner.value,
+			title: form.title.value,
+			status: 'New',
+			created: new Date(),
+		});
+		// clear the form for the next input
+		form.owner.value = '';
+		form.title.value = '';
+	}
+
+	render() {
+		return (
+			<div>
+				<form name="issueAdd" onSubmit={this.handleSubmit}>
+					<input type="text" name="owner" placeholder="Owner" />
+					<input type="text" name="title" placeholder="Title" />
+					<button>Add</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+
+
+// IssueList组件
+class IssueList extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			issues: []
+		};
+
+		// this.createTestIssue = this.createTestIssue.bind(this);
+		this.createIssue = this.createIssue.bind(this);
+		
+	}
+
+	componentDidMount() {
+		this.loadData();
+	}
+
+
+	loadData() {
+		// arrow-function会正确绑定当前上下文
+		setTimeout( () => {
+			// this.createTestIssue.bind(this), 
+			this.setState({
+				issues: issues
+			});
+		}, 500);
+	}
+
+
+
+	createIssue(newIssue) {
+		// 复制一份原数据
+		const newIssues = this.state.issues.slice();
+		// 设置新数据的id值
+		newIssue.id = this.state.issues.length + 1;
+		newIssues.push(newIssue);
+		// 通知React状态发生改变
+		this.setState({
+			issues: newIssues
+		});
+	}
+
+
+
+
+	createTestIssue() {
+		this.createIssue({
+			status: 'New',
+			owner: 'Pieta',
+			created: new Date(),
+			title: 'Completion date should be optional'
+		})
+	}
+
+
+
+
+	render() {
+		return (
+			<div>
+				<h1>Issue Tracker</h1>
+				<IssueFilter />
+				<hr />
+				<IssueTable issues={this.state.issues} />
+				<hr />
+				<IssueAdd createIssue={this.createIssue}/>
+			</div>
 		);
 	}
 }
